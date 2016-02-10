b2b3c85a2f4eff90877bc31df86e2822fa085802
neethu <neethump2014@gmail.com>
2016-02-10 18:10:56 +0530

    theme activity is added

---
+++ app/src/main/java/com/example/neethu/themeactivity/LayoutViewActivity.java
@@ -0,0 +1,29 @@
+package com.example.neethu.themeactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by neethu on 10/2/16.
+ */
+public class LayoutViewActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
